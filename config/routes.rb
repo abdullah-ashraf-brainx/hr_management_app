a4fae3a2a3c582b2aaabc9500f7c71ac75e88069
Abdullah <abdullah.ashraf@brainxtech.com>
2023-05-04 13:13:36 +0500

    login signup pages done

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, controllers: {
+    sessions: 'admin_users/sessions',
+    registrations: 'admin_users/registrations',
+    passwords: 'admin_users/passwords'
+  }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
